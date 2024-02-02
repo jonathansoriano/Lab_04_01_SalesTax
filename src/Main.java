43eb6399dd11499e04ab24213b9f92d5101b2a71
jonathansoriano <158002831+jonathansoriano@users.noreply.github.com>
2024-02-02 10:38:22 -0500

    First Program done

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 public class Main {
     public static void main(String [] args)
     {
-        double salesTax = .05;
+        double SALES_TAX = .05;
         double itemPrice = 10.50;
         double tax = 0.0;
+        double totalPrice = 0;
 
-        tax = itemPrice * salesTax;
+
+        tax = itemPrice * SALES_TAX;
+        totalPrice = tax + itemPrice;
 
 
         System.out.println("The sales tax for the item is " + tax + " and the price of the item is " + itemPrice);
+        System.out.println("The total price is " + totalPrice);
     }
 }
